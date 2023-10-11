9d57ea4d2ff21af31f4b2ddf384f73a3ca5b28ec
Rafael Muller <rafaelmuller.ads@gmail.com>
2023-10-10 21:38:35 -0300

    inserido imagens no menu e arquivo css / tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./agenda.html",
+    "./exercícios.html",
+    "./fisioterapeuta.html",
+    "./home.html",
+    "./index.html",
+    "./pacientes.html",
+    "./css/style.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
